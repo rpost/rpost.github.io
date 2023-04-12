fbdf41a3b441347e01dc3a895483a4aeb0f46edb
Radek Postołowicz <postolowicz@gmail.com>
2023-04-13 01:23:47 +0200

    Add draft

--- bin/dev_server.sh
+++ bin/dev_server.sh
@@ -1,1 +1,8 @@
-jekyll serve --livereload
+#jekyll serve --livereload --drafts
+
+docker run --rm \
+  --volume="$PWD:/srv/jekyll:Z" \
+  --publish [::1]:4000:4000 \
+  --publish [::1]:35729:35729 \
+  jekyll/jekyll:4.2.0 \
+  jekyll serve --livereload --drafts
